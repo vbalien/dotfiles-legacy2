a1aaa489dd571776e50baf6ab81a8bf32d9f6c0e
Jisu Kim <webmaster@alien.moe>
2020-07-23 13:37:53 +0900

    Add udev monitor

---
+++ linux/bspwm/layout_monitor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+bspc subscribe node_add | while read -r _ _ desktop _ node; do
+  num=$(bspc query -N -d $desktop | grep -f <(bspc query -N -n .leaf) | wc -l)
+  if [ $num = "1" ];then
+    xprop -id "$node" -f _PICOM_ROUNDED 32c -set _PICOM_ROUNDED 0
+  else
+    bspc query -N -d | while read -r node; do
+    xprop -id "$node" -remove _PICOM_ROUNDED
+    done
+  fi
+done &
+bspc subscribe node_remove | while read -r _ _ desktop _ node; do
+  num=$(bspc query -N -d $desktop | grep -f <(bspc query -N -n .leaf) | wc -l)
+  if [ $num = "1" ];then
+    bspc query -N -d | while read -r node; do
+    xprop -id "$node" -f _PICOM_ROUNDED 32c -set _PICOM_ROUNDED 0
+    done
+  fi
+done &
